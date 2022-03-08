e0105bb99f6a9274d3dac8d6cbc6427c27ac9f11
Mark Allibone <mark.allibone@rey-technology.com>
2022-03-08 21:03:28 +0100

    Fix the url for the identity server

--- Mobile/OidcSample/App.xaml.cs
+++ Mobile/OidcSample/App.xaml.cs
@@ -6,7 +6,11 @@ namespace OidcSample
     public partial class App : Application
     {
         // oidcxamarin101:/authenticated
-        public const string CallbackUri = "oidcxamarin";
+        public const string CallbackUriAzure = "oidcxamarin101";
+        public static readonly string CallbackSchemeAzure = $"{CallbackUri}://authenticated";
+        public static readonly string SignoutCallbackSchemeAzure = $"{CallbackUri}://signout-callback-oidc";
+        
+        public const string CallbackUri = "oidcxamarin101";
         public static readonly string CallbackScheme = $"{CallbackUri}://authenticated";
         public static readonly string SignoutCallbackScheme = $"{CallbackUri}://signout-callback-oidc";
         
